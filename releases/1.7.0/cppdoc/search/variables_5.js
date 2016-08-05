643c04975f070edb7b14da69c11dfbc7d00ed88d
Semen Boikov <sboikov@apache.org>
2016-08-05 15:41:02 +0000

    new Release (1.7.0)
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1755352 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releases/1.7.0/cppdoc/search/variables_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['typ',['typ',['../structignite_1_1binary_1_1BinaryType_3_01T_01_5_01_4.html#a5287f89167438904862ea239b8c1308f',1,'ignite::binary::BinaryType&lt; T * &gt;']]]
+];
