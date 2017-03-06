7f790a432ba9a30668aa7a6eadb653c4bbbef2bf
Semen Boikov <sboikov@apache.org>
2017-03-06 10:52:21 +0000

    new Release (1.9.0)
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1785628 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releases/1.9.0/dotnetdoc/class_apache_1_1_ignite_1_1_core_1_1_binary_1_1_binary_array_equality_comparer.js
@@ -0,0 +1,5 @@
+var class_apache_1_1_ignite_1_1_core_1_1_binary_1_1_binary_array_equality_comparer =
+[
+    [ "Equals", "class_apache_1_1_ignite_1_1_core_1_1_binary_1_1_binary_array_equality_comparer.html#a90840b3da8d8cf90a9e07296d06518b2", null ],
+    [ "GetHashCode", "class_apache_1_1_ignite_1_1_core_1_1_binary_1_1_binary_array_equality_comparer.html#ac4033347f8b6cb613166fb2c7c4806c5", null ]
+];
---
+++ releases/1.9.0/dotnetdoc/namespace_apache_1_1_ignite.js
@@ -0,0 +1,6 @@
+var namespace_apache_1_1_ignite =
+[
+    [ "AspNet", "namespace_apache_1_1_ignite_1_1_asp_net.html", "namespace_apache_1_1_ignite_1_1_asp_net" ],
+    [ "Core", "namespace_apache_1_1_ignite_1_1_core.html", "namespace_apache_1_1_ignite_1_1_core" ],
+    [ "Linq", "namespace_apache_1_1_ignite_1_1_linq.html", "namespace_apache_1_1_ignite_1_1_linq" ]
+];
