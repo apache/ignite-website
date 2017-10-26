f8978013d5aeb6fd69f596f65fe461a4194a2704
Anton Vinogradov <av@apache.org>
2017-10-26 13:55:28 +0000

    Removed test release
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1813402 13f79535-47bb-0310-9956-ffa450edef68

--- releases/2.3.0/cppdoc/search/enumvalues_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['backup',['BACKUP',['../structignite_1_1cache_1_1CachePeekMode.html#acd2d0e89b2c2c9d391060a89585bc2b2aa48147b4bdb14cb334fec9e695f27745',1,'ignite::cache::CachePeekMode']]]
-];
