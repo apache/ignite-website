7e788b0c6d2c30850d1372d50f5cdd150b3be3ee
Vladimir Ozerov <vozerov@apache.org>
2017-10-26 13:35:56 +0000

    new Release (2.3.0)
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1813399 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releases/2.3.0/cppdoc/search/classes_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['maptype',['MapType',['../structignite_1_1binary_1_1MapType.html',1,'ignite::binary']]],
+  ['mutablecacheentry',['MutableCacheEntry',['../classignite_1_1cache_1_1MutableCacheEntry.html',1,'ignite::cache']]]
+];
