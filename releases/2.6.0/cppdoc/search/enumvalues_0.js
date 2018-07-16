ea95af00af00ed6af01aa4d7a8e13e4005c21488
Andrey N. Gura <agura@apache.org>
2018-07-16 14:25:59 +0000

    new Release (2.6.0)
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1836045 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releases/2.6.0/cppdoc/search/enumvalues_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['active',['ACTIVE',['../structignite_1_1transactions_1_1TransactionState.html#a173faba612c6a4e91c0726140d8d43f3acefa9fd283ef4376d8053f4b7d68408b',1,'ignite::transactions::TransactionState']]],
+  ['all',['ALL',['../structignite_1_1cache_1_1CachePeekMode.html#acd2d0e89b2c2c9d391060a89585bc2b2ac963dd400d80659ced9e17f5474663c6',1,'ignite::cache::CachePeekMode']]],
+  ['array_5flist',['ARRAY_LIST',['../structignite_1_1binary_1_1CollectionType.html#a03610e138c65428dc81c08f62b7729bbacbc1082191c5452d77c8ffcfc012b8be',1,'ignite::binary::CollectionType']]]
+];
---
+++ releases/2.6.0/cppdoc/search/enumvalues_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['undefined',['UNDEFINED',['../structignite_1_1binary_1_1CollectionType.html#a03610e138c65428dc81c08f62b7729bba0dbcb37b8e35ac72ed89b53305ed3596',1,'ignite::binary::CollectionType::UNDEFINED()'],['../structignite_1_1binary_1_1MapType.html#a138cf8f1db233f607ea34aaa8a99b456ae64ff37323ba9bc49d3dd6e3133ac486',1,'ignite::binary::MapType::UNDEFINED()']]],
+  ['unknown',['UNKNOWN',['../structignite_1_1transactions_1_1TransactionState.html#a173faba612c6a4e91c0726140d8d43f3a560e630d2aed8035d97219d86389123c',1,'ignite::transactions::TransactionState']]]
+];
