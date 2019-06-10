f2dc5bcabac0e6197f4545a9b83f370ec4919c9a
Dmitry Pavlov <dpavlov@apache.org>
2019-06-10 14:42:45 +0000

    new Release (2.7.5)
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1860949 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releases/2.7.5/cppdoc/search/files_5.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['ignite_2eh',['ignite.h',['../ignite_8h.html',1,'']]],
+  ['ignite_5fbinding_2eh',['ignite_binding.h',['../ignite__binding_8h.html',1,'']]],
+  ['ignite_5fbinding_5fcontext_2eh',['ignite_binding_context.h',['../ignite__binding__context_8h.html',1,'']]],
+  ['ignite_5fclient_2eh',['ignite_client.h',['../ignite__client_8h.html',1,'']]],
+  ['ignite_5fclient_5fconfiguration_2eh',['ignite_client_configuration.h',['../ignite__client__configuration_8h.html',1,'']]],
+  ['ignite_5fconfiguration_2eh',['ignite_configuration.h',['../ignite__configuration_8h.html',1,'']]],
+  ['ignite_5ferror_2eh',['ignite_error.h',['../ignite__error_8h.html',1,'']]],
+  ['ignition_2eh',['ignition.h',['../ignition_8h.html',1,'']]]
+];
