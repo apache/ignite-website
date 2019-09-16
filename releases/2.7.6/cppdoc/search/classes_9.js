f0f0224a4d22bb2ee60d82ef4ac571069d3583c1
Alexey Goncharuk <agoncharuk@apache.org>
2019-09-16 16:43:49 +0000

    new Release (2.7.6)
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1867019 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releases/2.7.6/cppdoc/search/classes_9.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['querycursor',['QueryCursor',['../classignite_1_1cache_1_1query_1_1QueryCursor.html',1,'ignite::cache::query']]],
+  ['queryfieldscursor',['QueryFieldsCursor',['../classignite_1_1cache_1_1query_1_1QueryFieldsCursor.html',1,'ignite::cache::query']]],
+  ['queryfieldsrow',['QueryFieldsRow',['../classignite_1_1cache_1_1query_1_1QueryFieldsRow.html',1,'ignite::cache::query']]]
+];
