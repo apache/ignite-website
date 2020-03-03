004e6d936dec4a5f0d1a43904f4b764e504fba5c
Maxim Muzafarov <mmuzaf@apache.org>
2020-03-03 11:51:43 +0000

    new Release (2.8.0)
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1874718 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releases/2.8.0/cppdoc/search/typedefs_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['binaryenumdereferenced',['BinaryEnumDereferenced',['../structignite_1_1binary_1_1BinaryEnum_3_01T_01_5_01_4.html#a190ddba8542dfae87ae210a92865ffbf',1,'ignite::binary::BinaryEnum&lt; T * &gt;']]],
+  ['binarytypedereferenced',['BinaryTypeDereferenced',['../structignite_1_1binary_1_1BinaryType_3_01T_01_5_4.html#a6af48dd63474d8baf2756192597e9c10',1,'ignite::binary::BinaryType&lt; T *&gt;']]]
+];
