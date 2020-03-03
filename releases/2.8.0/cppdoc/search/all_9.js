839e325f8cf3211dcbd2feff9923efbe31499462
Maxim Muzafarov <mmuzaf@apache.org>
2020-03-03 17:02:58 +0000

    Removing redundant Release
    git-svn-id: https://svn.apache.org/repos/asf/ignite/site/trunk@1874742 13f79535-47bb-0310-9956-ffa450edef68

--- releases/2.8.0/cppdoc/search/all_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['key',['key',['../classignite_1_1cache_1_1CacheEntry.html#ae09bd00e6147075a96606db1987ef0d7',1,'ignite::cache::CacheEntry']]],
-  ['keytype',['KeyType',['../classignite_1_1thin_1_1cache_1_1CacheClient.html#a27d8e2533526122c277b1ea33d73f064',1,'ignite::thin::cache::CacheClient']]]
-];
