1844cf9878fcc6891fd58497178abc802db14c6b
Maxim Muzafarov <maxmuzaf@gmail.com>
2021-03-16 00:26:13 +0300

    Ignite 2.10.0 docs

---
+++ releases/2.10.0/cppdoc/search/classes_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['promise',['Promise',['../classignite_1_1common_1_1Promise.html',1,'ignite::common']]]
+];
---
+++ releases/2.10.0/cppdoc/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['apache_20ignite_20c_2b_2b',['Apache Ignite C++',['../index.html',1,'']]],
+  ['apache_20ignite_20thin_20c_2b_2b_20client',['Apache Ignite Thin C++ client',['../md__opt_buildagent_work_7111a7dcda6647fb_ignite_modules_platforms_cpp_thin-client_README.html',1,'']]]
+];
