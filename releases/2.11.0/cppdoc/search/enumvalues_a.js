bf117af1ba6cfcac144a8d5303cf7452febf92d1
Maxim Muzafarov <maxmuzaf@gmail.com>
2021-09-17 02:59:41 +0300

    Ignite 2.11.0 docs

---
+++ releases/2.11.0/cppdoc/search/enumvalues_a.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['read_5fcommitted',['READ_COMMITTED',['../structignite_1_1transactions_1_1TransactionIsolation.html#a3ac24463ce4e1f63186a974c33c6b3e3afedbd137a83cadbbff0daae1f10f4c53',1,'ignite::transactions::TransactionIsolation::READ_COMMITTED()'],['../structignite_1_1thin_1_1transactions_1_1TransactionIsolation.html#a469b243892db2781f60f5c3f79b4ed75aa3e0e30cfc972ff0ec578f56ee194c54',1,'ignite::thin::transactions::TransactionIsolation::READ_COMMITTED()']]],
+  ['repeatable_5fread',['REPEATABLE_READ',['../structignite_1_1transactions_1_1TransactionIsolation.html#a3ac24463ce4e1f63186a974c33c6b3e3a6f7ada83d09adcb5cf6d107c08f88dec',1,'ignite::transactions::TransactionIsolation::REPEATABLE_READ()'],['../structignite_1_1thin_1_1transactions_1_1TransactionIsolation.html#a469b243892db2781f60f5c3f79b4ed75adbe28bc0bc5be3717623fdd61aa494b9',1,'ignite::thin::transactions::TransactionIsolation::REPEATABLE_READ()']]],
+  ['require',['REQUIRE',['../structignite_1_1thin_1_1SslMode.html#ad952e796d54c3c7e4ff72ce84762af8aa64e9972c071d65ef713961fb8ad9381f',1,'ignite::thin::SslMode']]],
+  ['rolled_5fback',['ROLLED_BACK',['../structignite_1_1transactions_1_1TransactionState.html#a173faba612c6a4e91c0726140d8d43f3a344523f7d8aa9c72c0eba3d53cf9f707',1,'ignite::transactions::TransactionState']]],
+  ['rolling_5fback',['ROLLING_BACK',['../structignite_1_1transactions_1_1TransactionState.html#a173faba612c6a4e91c0726140d8d43f3a3661aba35d8598cf78de468907a65d48',1,'ignite::transactions::TransactionState']]]
+];
---
+++ releases/2.11.0/cppdoc/search/variables_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['maintenance',['maintenance',['../structignite_1_1IgniteProductVersion.html#ac5b10ff569b257e62cf7e37d182a3e4d',1,'ignite::IgniteProductVersion']]],
+  ['majornumber',['majorNumber',['../structignite_1_1IgniteProductVersion.html#a1de2b3b4649eaf28a1d6c7cd2668c33c',1,'ignite::IgniteProductVersion']]],
+  ['minornumber',['minorNumber',['../structignite_1_1IgniteProductVersion.html#a9298370a9b235db44004b56b2c4e845e',1,'ignite::IgniteProductVersion']]]
+];
