a88257eb48442214401ec6e977326f602b034116
Erlan Aytpaev <aytpaev@gmail.com>
2023-04-25 12:50:01 +0600

    IGNITE-19049: configure the building process

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,96 @@ import pug from 'gulp-pug';
 import replace from 'gulp-replace';
 import prettier from 'gulp-prettier';
 import gulpSitemap from 'gulp-sitemap';
-
+import File from 'vinyl';
+import collection from 'gulp-collection';
+import matter from 'gulp-gray-matter';
+import sort from 'gulp-sort';
+import fs from 'fs';
+import rename from 'gulp-rename';
+import through from'through2';
+import engine from 'pug';
 
 let pugPath = "";
+let contextTags = [];
 
+function loadTemplate(name) {
+    return new File({
+      path: name,
+      contents: fs.readFileSync(name)
+    });
+}
+
+function buffer() {
+    var files = [];
+    return through.obj(function(file, enc, next) {
+      files.push(file);
+      next(null);
+    }, function(cb) {
+      var stream = this;
+      files.forEach(function(file) {
+        stream.push(file);
+      });
+      cb();
+    });
+}
+
+const blog = () => {
+    var blogTemplate = loadTemplate('./_src/_components/templates/blog.pug');
+    var postTemplate = loadTemplate('./_src/_components/templates/post.pug');
+
+    return gulp.src('./_src/_blog/*.pug')
+        .pipe(matter())
+        // sort by published date
+        .pipe(sort({
+            comparator: function(file1, file2) {
+                return new Date(file2.data.date) - new Date(file1.data.date);
+            }
+        }))
+        .pipe(collection(':tags/:tag/:pager.idx/index.html', {
+            item: blogTemplate,
+            paginate: {limit: 10},
+            groupFn: function(group) {
+                contextTags = Object.keys(group)
+                // configure a new pager (for all blog posts) via using a virtual "blog" tag
+                let blog = {}
+                for (let tag in group) {
+                    for (let key in group[tag]) {
+                        blog[group[tag][key].path] = group[tag][key]
+                    }
+                }
+                group.blog = Object.values(blog)
+                group.blog.sort(function(file1, file2) {
+                    return new Date(file2.data.date) - new Date(file1.data.date);
+                })
+            }
+        }))
+        // buffer files before rendering to ensure context has been created
+        .pipe(buffer())
+        .pipe(through.obj(function(file, enc, next) {
+            // configure a blog post template
+            if (file.data.items) {
+                file.data.items.sort(function(file1, file2) {
+                    return new Date(file2.data.date) - new Date(file1.data.date);
+                })
+            }
+            else {
+                file.data.post = engine.compile(file.contents.toString())()
+                file.contents = postTemplate.contents
+                file.data.context = contextTags
+                file.data.format_date = new Date(file.data.date).toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})
+            }
+            next(null, file);
+        }))
+        .pipe(pug())
+        .pipe(rename(function(path) {
+            // configure blog folders' structure
+            path.dirname = path.dirname.replace('tags/', '');
+            path.dirname = path.dirname.replace('blog/0', '');
+            path.dirname = path.dirname.replace('blog/', '');
+            path.dirname = path.dirname.replace('/0', '');
+        }))
+        .pipe(gulp.dest('blog'));
+}
 
 //Compiler PUG -> HTML
 const html = () => {
@@ -34,6 +120,7 @@ const html = () => {
             console.log('▒ PUG→HTML: ' + file.path.replace(file.cwd, '') );
         })
         .pipe(gulp.dest('./')).on('end', (e) => {
+            blog()
         });
 }
 
