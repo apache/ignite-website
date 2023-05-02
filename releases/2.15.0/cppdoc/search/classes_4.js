00956efc0c8e46dfa1b2c7f664c9909fd3e0f8c9
Aleksey Plekhanov <plehanov.alex@gmail.com>
2023-05-02 18:47:23 +0300

    Ignite 2.15.0 docs

---
+++ releases/2.15.0/cppdoc/search/classes_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['future_727',['Future',['../classignite_1_1Future.html',1,'ignite']]],
+  ['future_3c_20common_3a_3aconcurrent_3a_3asharedpointer_3c_20t_20_3e_20_3e_728',['Future&lt; common::concurrent::SharedPointer&lt; T &gt; &gt;',['../classignite_1_1Future_3_01common_1_1concurrent_1_1SharedPointer_3_01T_01_4_01_4.html',1,'ignite']]],
+  ['future_3c_20void_20_3e_729',['Future&lt; void &gt;',['../classignite_1_1Future_3_01void_01_4.html',1,'ignite']]]
+];
---
+++ releases/2.15.0/cppdoc/search/classes_c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['scanquery_763',['ScanQuery',['../classignite_1_1thin_1_1cache_1_1query_1_1ScanQuery.html',1,'ignite::thin::cache::query::ScanQuery'],['../classignite_1_1cache_1_1query_1_1ScanQuery.html',1,'ignite::cache::query::ScanQuery']]],
+  ['sqlfieldsquery_764',['SqlFieldsQuery',['../classignite_1_1thin_1_1cache_1_1query_1_1SqlFieldsQuery.html',1,'ignite::thin::cache::query::SqlFieldsQuery'],['../classignite_1_1cache_1_1query_1_1SqlFieldsQuery.html',1,'ignite::cache::query::SqlFieldsQuery']]],
+  ['sqlquery_765',['SqlQuery',['../classignite_1_1cache_1_1query_1_1SqlQuery.html',1,'ignite::cache::query']]],
+  ['sslmode_766',['SslMode',['../structignite_1_1thin_1_1SslMode.html',1,'ignite::thin']]]
+];
