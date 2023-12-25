f50745ff5ff01ecf0b3547339e76f87ad013e539
NSAmelchev <nsamelchev@gmail.com>
2023-12-25 15:32:41 +0300

    Ignite 2.16.0 docs

---
+++ releases/2.16.0/cppdoc/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['attachhelper_675',['AttachHelper',['../classignite_1_1jni_1_1AttachHelper.html',1,'ignite::jni']]]
+];
---
+++ releases/2.16.0/cppdoc/search/classes_6.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['ignite_731',['Ignite',['../classignite_1_1Ignite.html',1,'ignite']]],
+  ['ignitebinding_732',['IgniteBinding',['../classignite_1_1IgniteBinding.html',1,'ignite']]],
+  ['ignitebindingcontext_733',['IgniteBindingContext',['../classignite_1_1IgniteBindingContext.html',1,'ignite']]],
+  ['igniteclient_734',['IgniteClient',['../classignite_1_1thin_1_1IgniteClient.html',1,'ignite::thin']]],
+  ['igniteclientconfiguration_735',['IgniteClientConfiguration',['../classignite_1_1thin_1_1IgniteClientConfiguration.html',1,'ignite::thin']]],
+  ['ignitecluster_736',['IgniteCluster',['../classignite_1_1cluster_1_1IgniteCluster.html',1,'ignite::cluster']]],
+  ['igniteconfiguration_737',['IgniteConfiguration',['../structignite_1_1IgniteConfiguration.html',1,'ignite']]],
+  ['igniteerror_738',['IgniteError',['../classignite_1_1IgniteError.html',1,'ignite']]],
+  ['ignitepredicate_739',['IgnitePredicate',['../classignite_1_1IgnitePredicate.html',1,'ignite']]],
+  ['igniteproductversion_740',['IgniteProductVersion',['../structignite_1_1IgniteProductVersion.html',1,'ignite']]],
+  ['ignition_741',['Ignition',['../classignite_1_1Ignition.html',1,'ignite']]]
+];
