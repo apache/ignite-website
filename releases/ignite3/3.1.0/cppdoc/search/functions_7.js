e393f9a43e4fe3523c31f2f3dff382dd0e74ed3f
Evgeniy Stanilovskiy <stanilovsky@gmail.com>
2025-10-30 13:42:37 +0300

    IGNITE-26871 Update platforms documentation (#269)

---
+++ releases/ignite3/3.1.0/cppdoc/search/functions_7.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['has_5ferror_0',['has_error',['../classignite_1_1ignite__result.html#a9c79b16abc226c0100a1eba40cdbc88d',1,'ignite::ignite_result::has_error()'],['../classignite_1_1ignite__result_3_01void_01_4.html#aab640f5c0adad30cd218436f215d1246',1,'ignite::ignite_result&lt; void &gt;::has_error()']]],
+  ['has_5fmore_5fpages_1',['has_more_pages',['../classignite_1_1result__set.html#a95577d8f9697cca64e3912be49765365',1,'ignite::result_set']]],
+  ['has_5frowset_2',['has_rowset',['../classignite_1_1result__set.html#a02c3dfedfb3d2bcdd47d91ffa8af5304',1,'ignite::result_set']]],
+  ['has_5fvalue_3',['has_value',['../classignite_1_1ignite__result.html#a74ca153ebb89935d60e3e1ca37a02b46',1,'ignite::ignite_result']]]
+];
