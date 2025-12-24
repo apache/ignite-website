b3cbe8216755dafd64d33c04691bb8bea78093e6
Michael Aglietti <maglietti@me.com>
2025-12-24 11:01:04 -0600

    IGNITE-27450 Update the suggested-site with a combined version of the ignite-website, Ignite 2 docs, and Ignite 3 docs for review

---
+++ suggested-site/docs/ignite3/api/cpp/search/all_c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['malformed_5fcanonical_5fname_5ffixture_0',['malformed_canonical_name_fixture',['../classmalformed__canonical__name__fixture.html',1,'']]],
+  ['malformed_5fsimple_5fname_5ffixture_1',['malformed_simple_name_fixture',['../classmalformed__simple__name__fixture.html',1,'']]],
+  ['metadata_2',['metadata',['../classignite_1_1result__set.html#a39c33546aa88754a6fa29ca94f2bd1d4',1,'ignite::result_set']]],
+  ['multiply_3',['multiply',['../classignite_1_1big__decimal.html#a91ac5d28b26e4fd17911509b8f5408d9',1,'ignite::big_decimal::multiply()'],['../classignite_1_1big__integer.html#ac7411d69fcc183d1c33bdf2ca2e663b7',1,'ignite::big_integer::multiply()']]]
+];
