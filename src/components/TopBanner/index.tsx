64f29c327c96fa456f4a69c80d13f13238ed82de
IgGusev <igusev@gridgain.com>
2026-02-03 18:04:04 +0200

    Changed top banner

--- src/components/TopBanner/index.tsx
+++ src/components/TopBanner/index.tsx
@@ -14,13 +14,13 @@ export default function TopBanner(): JSX.Element {
   return (
     <div id="promotion-bar" className={styles.promotionBar}>
       <a
-        href="https://ignite-summit.org/2025/?utm_medium=website-direct&utm_source=apache_ignite&utm_term=ignite-summit&utm_content=event-hosted&utm_campaign=2023-06-06-ignite-summit-nam"
+        href="https://ignite-summit.org/?utm_medium=website-direct&utm_source=apache_ignite&utm_term=ignite-summit&utm_content=event-hosted"
         target="_blank"
         rel="noopener noreferrer"
-        title="Ignite Summit 2025"
+        title="Ignite Summit 2026"
         className={styles.promotionLink}
       >
-        Ignite Summit 2025 — Watch on demand &nbsp;
+        Save the Date: Ignite Summit 2026 &nbsp;
         <span className={styles.more}>
           <img src={arrowIcon} alt="" />
         </span>
